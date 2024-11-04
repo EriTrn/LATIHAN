76f84992f196963bbc0587b5a028bca644653fe9
Eri Trn <rosariogt0050@gmail.com>
2024-11-04 21:27:11 +0700

    10 contoh if-else python

---
+++ main10.py
@@ -0,0 +1,69 @@
+# Contoh 1
+age = 20
+if age >= 18:
+    print("Dewasa")
+else:
+    print("Belum Dewasa")
+
+# Contoh 2
+temperature = 30
+if temperature > 25:
+    print("Cuaca Panas")
+else:
+    print("Cuaca Sejuk")
+
+# Contoh 3
+number = 7
+if number % 2 == 0:
+    print("Genap")
+else:
+    print("Ganjil")
+
+# Contoh 4
+password = "secure123"
+if len(password) >= 8:
+    print("Password aman")
+else:
+    print("Password terlalu pendek")
+
+# Contoh 5
+x = 5
+if x > 0:
+    print("Positif")
+else:
+    print("Negatif atau Nol")
+
+# Contoh 6
+day = "Minggu"
+if day == "Sabtu" or day == "Minggu":
+    print("Akhir Pekan")
+else:
+    print("Hari Kerja")
+
+# Contoh 7
+score = 85
+if score >= 75:
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+
+# Contoh 8
+hours_worked = 45
+if hours_worked > 40:
+    print("Lembur")
+else:
+    print("Normal")
+
+# Contoh 9
+height = 180
+if height > 170:
+    print("Tinggi")
+else:
+    print("Pendek")
+
+# Contoh 10
+balance = 5000
+if balance >= 10000:
+    print("Saldo Cukup")
+else:
+    print("Saldo Tidak Cukup")
