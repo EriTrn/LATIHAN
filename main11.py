403f022d18a681d64960269b5b6b4b1f38ee843e
Eri Trn <rosariogt0050@gmail.com>
2024-11-05 23:21:23 +0700

    10 contoh if-elif-else

---
+++ main11.py
@@ -0,0 +1,97 @@
+# Contoh 1
+grade = 85
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
+
+# Contoh 2
+day = "Rabu"
+if day == "Senin":
+    print("Hari Senin")
+elif day == "Selasa":
+    print("Hari Selasa")
+elif day == "Rabu":
+    print("Hari Rabu")
+else:
+    print("Hari lainnya")
+
+# Contoh 3
+time = 14
+if time < 12:
+    print("Pagi")
+elif time < 18:
+    print("Siang")
+else:
+    print("Malam")
+
+# Contoh 4
+temperature = 32
+if temperature < 15:
+    print("Dingin")
+elif 15 <= temperature <= 25:
+    print("Sejuk")
+else:
+    print("Panas")
+
+# Contoh 5
+weight = 70
+if weight < 50:
+    print("Underweight")
+elif 50 <= weight < 75:
+    print("Normal")
+else:
+    print("Overweight")
+
+# Contoh 6
+speed = 120
+if speed < 60:
+    print("Lambat")
+elif speed < 100:
+    print("Sedang")
+else:
+    print("Cepat")
+
+# Contoh 7
+points = 50
+if points >= 100:
+    print("Gold")
+elif 50 <= points < 100:
+    print("Silver")
+else:
+    print("Bronze")
+
+# Contoh 8
+grade = "B"
+if grade == "A":
+    print("Sangat Baik")
+elif grade == "B":
+    print("Baik")
+elif grade == "C":
+    print("Cukup")
+else:
+    print("Kurang")
+
+# Contoh 9
+month = "April"
+if month in ["Desember", "Januari", "Februari"]:
+    print("Musim Dingin")
+elif month in ["Maret", "April", "Mei"]:
+    print("Musim Semi")
+else:
+    print("Musim lainnya")
+
+# Contoh 10
+daytime = "pagi"
+if daytime == "pagi":
+    print("Selamat Pagi")
+elif daytime == "siang":
+    print("Selamat Siang")
+else:
+    print("Selamat Malam")
