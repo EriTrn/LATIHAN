c4294aa2ddae3e099c849103bc910c724f4d3f88
Eri Trn <rosariogt0050@gmail.com>
2024-11-07 16:24:09 +0700

    10 Contoh penggunaan nested-if

---
+++ main12.py
@@ -0,0 +1,107 @@
+# Contoh 1
+age = 25
+if age >= 18:
+    if age < 60:
+        print("Dewasa")
+    else:
+        print("Lansia")
+else:
+    print("Belum Dewasa")
+
+# Contoh 2
+score = 85
+if score >= 70:
+    if score >= 90:
+        print("Nilai A")
+    elif score >= 80:
+        print("Nilai B")
+    else:
+        print("Nilai C")
+else:
+    print("Tidak Lulus")
+
+# Contoh 3
+temperature = 20
+if temperature > 15:
+    if temperature < 30:
+        print("Cuaca Hangat")
+    else:
+        print("Cuaca Panas")
+else:
+    print("Cuaca Dingin")
+
+# Contoh 4
+speed = 90
+if speed > 60:
+    if speed > 100:
+        print("Sangat Cepat")
+    else:
+        print("Cukup Cepat")
+else:
+    print("Lambat")
+
+# Contoh 5
+balance = 12000
+if balance > 5000:
+    if balance >= 10000:
+        print("Saldo Cukup")
+    else:
+        print("Saldo Menengah")
+else:
+    print("Saldo Rendah")
+
+# Contoh 6
+height = 180
+if height > 150:
+    if height > 170:
+        print("Tinggi")
+    else:
+        print("Sedang")
+else:
+    print("Pendek")
+
+# Contoh 7
+time = 15
+if time >= 0:
+    if time < 12:
+        print("Pagi")
+    elif time < 18:
+        print("Siang")
+    else:
+        print("Malam")
+else:
+    print("Waktu Tidak Valid")
+
+# Contoh 8
+grade = "B"
+if grade == "A":
+    print("Sangat Baik")
+elif grade == "B":
+    if grade >= "B+":
+        print("Baik Sekali")
+    else:
+        print("Baik")
+else:
+    print("Kurang")
+
+# Contoh 9
+age = 45
+if age >= 18:
+    if age <= 30:
+        print("Dewasa Muda")
+    elif age <= 50:
+        print("Dewasa")
+    else:
+        print("Lansia")
+else:
+    print("Anak-anak")
+
+# Contoh 10
+weight = 68
+if weight > 50:
+    if weight <= 70:
+        print("Normal")
+    else:
+        print("Berat")
+else:
+    print("Ringan")
